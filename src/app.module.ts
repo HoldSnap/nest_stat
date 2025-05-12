7e43a2dbdbaf05879795aa60f215f244b5929b81
Stepanov Mikhail <volodya-stepanov-68@gmail.ru>
2025-05-12 17:38:28 +0300

    fix: change import

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,11 @@ import { CategoryModule } from './category/category.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { TransactionModule } from './transaction/transaction.module';
-import { CategoriesController } from './categories/categories.controller';
-import { CategoriesController } from './categories/categories.controller';
+import { CategoryController } from './category/category.controller';
 
 @Module({
   imports: [PrismaModule, CategoryModule, AuthModule, TransactionModule],
-  controllers: [CategoriesController],
+  controllers: [CategoryController],
   providers: [CategoryService],
 })
 export class AppModule {}
